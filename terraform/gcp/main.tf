6afced827276bfbbb064aea831101b9d98ee766a
Dmitry Fomin <dmitry@postgres.ai>
2022-01-18 16:50:52 +0000

    1

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "dmitry-terraform-instance"
   machine_type = "f1-micro"
 
   boot_disk {
@@ -17,6 +17,19 @@ resource "google_compute_instance" "vm_instance" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
+  name                    = "terraform-dle-network"
   auto_create_subnetworks = "true"
 }
+
+resource "google_compute_firewall" "firewall_dle" {
+  name    = "terraform-dle-fw-rule"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "8080", "1000-2000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
